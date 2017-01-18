fc32a024d86e5e885fd465684b5ab0dbd7eb7e9b
Alex Markowitz <admarko@gmail.com>
2017-01-18 17:20:04 -0600

    Rock Paper Scissors
    Simple Rock-Paper-Scissors game built using Ruby

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -11,10 +11,51 @@ class GameController < ApplicationController
 
     # In the end, make sure you assign the correct values to the
     #   following two variables:
+    cmove = rand(3)
+    if cmove < 1
+      cmove = "Rock"
+    elsif cmove <2
+      cmove = "Paper"
+    else
+      cmove = "Scissors"
+    end
+    @computer_move = cmove
 
-    @computer_move = "Replace this string with the correct value."
+    res = ""
 
-    @result = "Replace this string with the correct value."
+    if ((@user_move == "rock" && cmove == "Rock") ||
+       (@user_move == "paper" && cmove == "Paper") ||
+       (@user_move == "scissors" && cmove == "Scissors"))
+       res = "tie"
+     elsif @user_move == "rock"
+       if cmove = "paper"
+         res = "lose"
+       else
+         res = "win"
+       end
+     elsif @user_move == "paper"
+       if cmove = "rock"
+         res = "win"
+       else
+         res = "lose"
+       end
+     else
+       if cmove = "rock"
+         res = "lose"
+       else
+         res = "win"
+       end
+    end
+
+    if res == "win"
+      res = "WON!!! 😀  "
+    elsif res == "lose"
+      res = "LOST! 😭  "
+    else
+      res = "TIED! 😑 "
+    end
+
+    @result = res
 
     # ===============================================================
     # Your code goes above.
